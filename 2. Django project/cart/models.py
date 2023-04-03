f0d3d538adae0eb970f8a6ca88085e8e6fe403cd
zed-chi <zed-chi@mail.ru>
2023-04-03 16:50:25 +0300

    added cart model

--- 2. Django project/cart/models.py
+++ 2. Django project/cart/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.conf import settings
 
+from products.models import Product
+
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete)
+
+    def flush(self):
+        try:
+            items = self.items.all()
+            for i in items:
+                i.delete()
+        except:
+            pass
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete)
+    product = models.ForeignKey(to, on_delete)
+    quantity = models.IntegerField()
+    item_price = models.DecimalField()
