7fce4d05bb1db64d1147cc45c48e1c23c680e331
zed-chi <zed-chi@mail.ru>
2023-04-03 20:13:00 +0300

    custom widget just for fun

---
+++ 2. Django project/products/form_widgets.py
@@ -0,0 +1,41 @@
+from django.forms.widgets import Widget
+from .models import Category
+import json
+from django.conf import settings
+
+
+class CategoryListWidget(Widget):
+    """
+    Виджет выбора цепочки категорий
+    """
+
+    cat_ids_string = ""
+    cat_ids = []
+    template_name = "products/widgets/category-list.html"
+
+    def format_value(self, value: str):
+        """Разделить"""
+        if value:
+            return [int(x) for x in value.split(" ") if x.strip()]
+
+    def js_value(self, ids):
+        items = [{"id": _id, "title": "qwe"} for _id in ids]
+        return json.dumps(items)
+
+    def get_context(self, name, value, attrs):
+        formatted_value = self.format_value(value)
+        js_value = self.js_value(formatted_value)
+        last_id = formatted_value[-1]
+        return {
+            "widget": {
+                "name": name,
+                "is_hidden": self.is_hidden,
+                "required": self.is_required,
+                "value": value,
+                "js_value": js_value,
+                "attrs": self.build_attrs(self.attrs, attrs),
+                "last_id": last_id,
+                "template_name": self.template_name,
+                "catUrl": settings.CATEGORIES_URL,
+            },
+        }
