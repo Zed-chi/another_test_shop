1835541cb96f2a09b38cb20cfaaac8c6e1fe6129
zed-chi <zed-chi@mail.ru>
2023-04-03 23:52:42 +0300

    verbose naming

--- 2. Django project/products/models.py
+++ 2. Django project/products/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(
+        max_length=255, unique=True, verbose_name="Название"
+    )
     slug = models.SlugField(unique=True)
     parent = models.ForeignKey(
-        "Category", on_delete=models.SET_NULL, null=True, blank=True
+        "Category",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Надкатегория",
     )
     image = models.ImageField(
         upload_to="category",
@@ -13,15 +19,31 @@ class Category(models.Model):
         height_field=200,
         null=True,
         blank=True,
+        verbose_name="Изображение",
     )
+
+    def __str__(self):
+        return f"#{self.id} - {self.title}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["id"]
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
-    category_path = models.TextField()
+    title = models.CharField(
+        max_length=255, verbose_name="Название", unique=True
+    )
+    category_path = models.TextField(verbose_name="Входит в категории")
 
-    def get_categories(self):
-        return [int(x) for x in self.category_path.split(" ")]
+    def __str__(self):
+        return f"#{self.id} - {self.title}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["id"]
 
 
 class CategoryProductRel(models.Model):
@@ -31,3 +53,14 @@ class CategoryProductRel(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="items"
     )
+
+    class Meta:
+        verbose_name = "Связь Продукт-Категория"
+        verbose_name_plural = "Связи Продукт-Категория"
+        unique_together = (
+            "category",
+            "product",
+        )
+
+    def __str__(self):
+        return f"[{self.category}] - {self.product.title}"
