4704cf16bf3a91ae9010169437466385db13d85a
zed-chi <zed-chi@mail.ru>
2023-04-03 22:51:45 +0300

    added api route

--- 2. Django project/shop/urls.py
+++ 2. Django project/shop/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #
+    path("api/", include("api.urls")),
+    path("", include("products.urls")),
     path("", include("main.urls")),
 ]
 
