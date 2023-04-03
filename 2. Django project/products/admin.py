9c449ac856a738cbee410fdd98957e22868ad637
zed-chi <zed-chi@mail.ru>
2023-04-03 20:13:27 +0300

    registered product model in admin dash

--- 2. Django project/products/admin.py
+++ 2. Django project/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category
+from .forms import ProductForm
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductForm
