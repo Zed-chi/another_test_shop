9fa68bc1a5c81d1c432ca05559f57d207863692c
zed-chi <zed-chi@mail.ru>
2023-04-03 16:50:10 +0300

    added category and product models

--- 2. Django project/products/models.py
+++ 2. Django project/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+    parent = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    image = models.ImageField(
+        upload_to="category",
+        width_field=200,
+        height_field=200,
+        null=True,
+        blank=True,
+    )
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(to, on_delete)
