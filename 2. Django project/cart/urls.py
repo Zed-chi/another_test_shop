a10bd12a6bd4f030abf83384dce839e1920b49fd
zed-chi <zed-chi@mail.ru>
2023-04-04 19:44:26 +0300

    basic routes for cart

--- 2. Django project/cart/urls.py
+++ 2. Django project/cart/urls.py
@@ -5,5 +5,10 @@ from . import views
 
 app_name = "cart"
 urlpatterns = [
-    path("add/<int:p_id>", views.add_product, name="add_product"),
+    path("flush", views.flush_cart, name="flush_cart"),
+    path("add/<slug:slug>", views.add_product, name="add_product"),
+    path("inc/<slug:slug>", views.increase_product_num, name="inc_product"),
+    path("dec/<slug:slug>", views.decrease_product_num, name="dec_product"),
+    path("del/<slug:slug>", views.delete_product, name="delete_product"),
+    path("", views.cart_home, name="detail"),
 ]
