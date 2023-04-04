efa894e9bd2e0624c800651de8525b1ea0db98cc
zed-chi <zed-chi@mail.ru>
2023-04-04 21:56:58 +0300

    dump view

--- 2. Django project/orders/views.py
+++ 2. Django project/orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def checkout(req):
+    cart_id = req.POST.get("cart_id", None)
+    if not cart_id:
+        return redirect("cart:home")
+    cart = Cart.objects.get(pk=cart_id)
