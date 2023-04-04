22a8b924d28586f04827bf9454ce46353ebbdc62
zed-chi <zed-chi@mail.ru>
2023-04-04 20:16:51 +0300

    changed field

--- 2. Django project/cart/models.py
+++ 2. Django project/cart/models.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart"
     )
 
