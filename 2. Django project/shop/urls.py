972813d28427025b33f17007e116b6f9f0f7afb9
zed-chi <zed-chi@mail.ru>
2023-04-05 21:17:17 +0300

    added slash in the end

--- 2. Django project/shop/urls.py
+++ 2. Django project/shop/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("cart", include("cart.urls")),
+    path("orders/", include("orders.urls")),
+    path("cart/", include("cart.urls")),
     path("accounts/", include("accounts.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
