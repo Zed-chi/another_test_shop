c546269c0f3edb72d9e1c8e4787dd19804e970dc
zed-chi <zed-chi@mail.ru>
2023-04-05 13:58:22 +0300

    Added Item model

--- 2. Django project/orders/models.py
+++ 2. Django project/orders/models.py
@@ -17,11 +17,24 @@ class Order(models.Model):
     )
     address = models.TextField()
     user = model.ForeignKey(settings.AUTH_USER_MODEL, on_delete)
-    status = models.CharField(
-        max_length=255, choices=STATUSES, default="draft"
-    )
+    status = models.CharField(max_length=255, choices=STATUSES, default="draft")
     total_price = models.DecimalField(decimal_places=2, max_digits=10)
     shipping_price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
         return str(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="order_items"
+    )
+    quantity = models.IntegerField(default=1)
+    item_price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    class Meta:
+        unique_together = ["order", "product"]
+
+    def total_price(self):
+        return self.item_price * self.quantity
