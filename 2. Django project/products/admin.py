61121634f32062e5378a277d2c8313addeb7dbfa
zed-chi <zed-chi@mail.ru>
2023-04-05 14:23:14 +0300

    Added inline image to admin

--- 2. Django project/products/admin.py
+++ 2. Django project/products/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Product, Category, CategoryProductRel
+from .models import Product, Category, CategoryProductRel, ProductImage
 from .forms import ProductForm
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 3
+    verbose_name = "Изображение продукта"
+    verbose_name_plural = "Изображения продукта"
 
 
 @admin.register(Product)
@@ -10,6 +17,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ["available", "created_at"]
     prepopulated_fields = {"slug": ["title"]}
     list_editable = ["price", "available"]
+    inlines = [ProductImageInline]
 
     def save_model(self, request, obj, form, change):
         """Additional step for:
@@ -17,9 +25,7 @@ class ProductAdmin(admin.ModelAdmin):
         - updating relations
         """
         super().save_model(request, obj, form, change)
-        category_relations = CategoryProductRel.objects.filter(
-            product_id=obj.id
-        )
+        category_relations = CategoryProductRel.objects.filter(product_id=obj.id)
         relations_ids = [i.category.id for i in category_relations]
         posted_ids = [int(i) for i in obj.category_path.split(" ")]
         for rel in category_relations:
@@ -28,9 +34,7 @@ class ProductAdmin(admin.ModelAdmin):
         for _id in posted_ids:
             if _id not in relations_ids:
                 category = Category.objects.get(id=_id)
-                CategoryProductRel.objects.create(
-                    product=obj, category=category
-                )
+                CategoryProductRel.objects.create(product=obj, category=category)
 
 
 @admin.register(Category)
