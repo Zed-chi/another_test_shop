d1b279f82907bb39ff9fad24b66e39fa282b862f
zed-chi <zed-chi@mail.ru>
2023-04-05 09:40:05 +0300

    added new user models

--- 2. Django project/accounts/models.py
+++ 2. Django project/accounts/models.py
@@ -1,3 +1,62 @@
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
+from cart.models import Cart
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("Email must be specified")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        cart = Cart.objects.create(user=user)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Profile model"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "name",
+    ]
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
+
+    def __str__(self):
+        return f"Profile for {self.user.name}"
