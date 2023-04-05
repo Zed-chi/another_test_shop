f05f3a83ad34c9ffdba721345b5c72c4994d68c8
zed-chi <zed-chi@mail.ru>
2023-04-05 09:38:58 +0300

    added routes for accounts app

--- 2. Django project/shop/urls.py
+++ 2. Django project/shop/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("cart", include("cart.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("cart", include("cart.urls")),
     path("", include("products.urls")),
     path("", include("main.urls")),
 ]
