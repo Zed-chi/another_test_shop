97c649d094612f6cf0239fe16a3ba1d4458ffebd
zed-chi <zed-chi@mail.ru>
2023-04-05 21:16:37 +0300

    shipping form

---
+++ 2. Django project/orders/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Order, OrderItem
+
+
+class OrderShippingForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            "payment",
+            "firstname",
+            "lastname",
+            "phonenumber",
+            "comment",
+            "address",
+        ]
+
+    # payment = forms.ChoiceField()
