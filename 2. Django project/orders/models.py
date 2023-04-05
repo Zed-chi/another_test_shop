bc1052a68a5818878f47e4943adebbaf78853974
zed-chi <zed-chi@mail.ru>
2023-04-05 13:56:13 +0300

    Changed cart field to user relation

--- 2. Django project/orders/models.py
+++ 2. Django project/orders/models.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.conf import settings
+from cart.models import Cart
 
-# Create your models here.
+
 class Order(models.Model):
     STATUSES = (
         (
-            "created",
-            "created",
+            "draft",
+            "draft",
         ),
         ("paid", "paid"),
         ("shipped", "shipped"),
+        ("delivered", "delivered"),
         ("refunding", "refunding"),
         ("refunded", "refunded"),
     )
     address = models.TextField()
-    shop_cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    user = model.ForeignKey(settings.AUTH_USER_MODEL, on_delete)
     status = models.CharField(
-        max_length=255, choices=STATUSES, default="created"
+        max_length=255, choices=STATUSES, default="draft"
     )
     total_price = models.DecimalField(decimal_places=2, max_digits=10)
     shipping_price = models.DecimalField(decimal_places=2, max_digits=10)
