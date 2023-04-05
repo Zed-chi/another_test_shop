50bb42b90b085fb09f4e38804921e03236420743
zed-chi <zed-chi@mail.ru>
2023-04-05 09:37:50 +0300

    registration login form

---
+++ 2. Django project/accounts/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+# from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
+
+
+class UserRegistrationForm(forms.ModelForm):
+    name = forms.CharField(
+        label="никнейм",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    email = forms.EmailField(
+        label="почта",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+    )
+
+    class Meta:
+        model = UserProfile
+        fields = ["name", "email", "password"]
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        label="почта",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserProfile
+        fields = ["email", "password"]
