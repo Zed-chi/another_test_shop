141a57f42fbde860bbf4f64d8d95e1c79c95cad3
zed-chi <zed-chi@mail.ru>
2023-04-05 23:33:58 +0300

    dump pay views

--- 2. Django project/orders/views.py
+++ 2. Django project/orders/views.py
@@ -37,7 +37,7 @@ def complete(request):
                 status="draft",
                 shipping_price=SHIPPING_PRICE,
             )
-            for cart_item in request.user.cart.items:
+            for cart_item in request.user.cart.items.all():
                 OrderItem.objects.create(
                     order=order,
                     product=cart_item.product,
@@ -45,9 +45,18 @@ def complete(request):
                     item_price=cart_item.product.price,
                 )
             order.total_price = order.get_full_price()
+            if cd["payment"] == "CASH":
+                order.status = "need_approval"
             order.save()
-        except:
-            pass
+            request.user.cart.flush()
+            messages.success(request, "Order created")
+            return redirect(reverse("orders:detail", kwargs={"pk": order.id}))
+        except Exception as e:
+            raise Exception(e)
+            messages.error(request, e)
+    else:
+        messages.error(request, "invalid form.")
+    return redirect("cart:detail")
 
 
 @login_required
@@ -61,13 +70,28 @@ def preorder(request):
 
 
 @login_required
-def pay_order_via_card():
-    pass
+def pay_order_via_card(request, pk):
+    if request.method != "POST":
+        messages.error(request, "Неверный метод запроса")
+        return redirect(reverse("orders:detail", kwargs={"pk": pk}))
+    return render(request, "orders/card-pay-dump.html", {"pk": pk})
+
+
+@login_required
+def dump_gate(request, pk):
+    if request.method != "POST":
+        messages.error(request, "Неверный метод запроса")
+    order = get_object_or_404(Order, pk=pk, user=request.user)
+    order.status = "paid"
+    order.save()
+    messages.success(request, "Заказ оплачен")
+    return redirect(reverse("orders:detail", kwargs={"pk": pk}))
 
 
 @login_required
 def order_detail(request, pk):
-    pass
+    order = get_object_or_404(Order, pk=pk, user=request.user)
+    return render(request, "orders/detail.html", {"order": order})
 
 
 @login_required
