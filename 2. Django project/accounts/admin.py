b0d6f001af1b9a11d667d1729112fa3ad8ae9ee1
zed-chi <zed-chi@mail.ru>
2023-04-06 10:35:45 +0300

    changed model name

--- 2. Django project/accounts/admin.py
+++ 2. Django project/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Profile, UserProfile
+from .models import ProfileInfo, UserProfile
 
 admin.site.register(UserProfile)
-admin.site.register(Profile)
+admin.site.register(ProfileInfo)
