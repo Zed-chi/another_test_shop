01f927dc3426b49a7d05ff133452ce71d0f23b17
zed-chi <zed-chi@mail.ru>
2023-04-06 16:01:56 +0300

    added auth vars

--- 2. Django project/shop/settings.py
+++ 2. Django project/shop/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIRS = [BASE_DIR / env.str("STATIC_DIR", "__static")]
 MEDIA_URL = "media/"
 MEDIA_ROOT = env.str("MEDIA_ROOT", "__media")
 
+LOGIN_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
