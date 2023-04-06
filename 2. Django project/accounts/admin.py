186bfb55f11d72397c3319c6661dd444b87bbba2
zed-chi <zed-chi@mail.ru>
2023-04-06 12:39:49 +0300

    changed user admin view

--- 2. Django project/accounts/admin.py
+++ 2. Django project/accounts/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import ProfileInfo, UserProfile
 
-admin.site.register(UserProfile)
+
 admin.site.register(ProfileInfo)
+
+
+@admin.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "id"]
