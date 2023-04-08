4b172082ba6fb0dbfb51856745ba6ed7d8a18c0b
zed-chi <zed-chi@mail.ru>
2023-04-08 12:38:53 +0300

    updated user detail view

--- 2. Django project/accounts/views.py
+++ 2. Django project/accounts/views.py
@@ -2,10 +2,10 @@ from cart.models import Cart
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 
-from .forms import LoginForm, UserRegistrationForm
-from .models import UserProfile
+from .forms import LoginForm, UserRegistrationForm, ProfileForm
+from .models import UserProfile, ProfileInfo
 
 
 def register(req):
@@ -16,9 +16,8 @@ def register(req):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Cart.objects.create(user=new_user)
-            return render(
-                req, "accounts/register_done.html", {"new_user": new_user}
-            )
+            ProfileInfo.objects.create(user=new_user)
+            return render(req, "accounts/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
     return render(req, "accounts/register.html", {"user_form": user_form})
@@ -29,11 +28,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(
-                request, email=cd["email"], password=cd["password"]
-            )
-            print(f"=== {user } ===")
-
+            user = authenticate(request, email=cd["email"], password=cd["password"])
             if user:
                 login(request, user)
                 return redirect("main:homepage")
@@ -44,8 +39,13 @@ def user_login(request):
 
 
 @login_required
-def user_detail(req, username):
-    user = get_object_or_404(UserProfile, username=username, is_active=True)
-    return render(
-        req, "accounts/detail.html", {"section": "people", "user": user}
-    )
+def user_detail(request):
+    form = ProfileForm()
+    if request.method == "GET":
+        form = ProfileForm()
+        return render(request, "accounts/detail.html", {"form": form})
+    form = ProfileForm(date=request.POST)
+    if not form.is_valid():
+        form.save()
+
+    return redirect(reverse("accounts:detail"))
